e48522f09f76d4f05cc4ae7eec4541cc73384f30
ives560 <kingwan560@163.com>
2017-03-22 19:25:20 +0800

    scroll chart

---
+++ customchart.cpp
@@ -0,0 +1,6 @@
+#include "customchart.h"
+
+CustomChart::CustomChart()
+{
+
+}
---
+++ customchart.h
@@ -0,0 +1,11 @@
+#ifndef CUSTOMCHART_H
+#define CUSTOMCHART_H
+
+
+class CustomChart : public QChart
+{
+public:
+    CustomChart();
+};
+
+#endif // CUSTOMCHART_H
